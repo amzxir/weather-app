e994b9c498fe337ccabee4a45ebbc1e775bacc14
amzxir <amirreza79ahmadi@gmail.com>
2023-12-08 23:31:22 +0330

    qwe

--- src/component/layouts.jsx
+++ src/component/layouts.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Layouts() {
     return (
-        <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae laborum odit fugiat voluptates eum consequatur sed dolores ut corporis quam nam similique temporibus, facere hic voluptatem accusamus voluptatibus animi laudantium.
+        <div className="container">
+            
         </div>
     )
 }
