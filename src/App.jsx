2fd6dea6241587ad69a60e288cda4e4b2b40cf53
amzxir <amirreza79ahmadi@gmail.com>
2023-12-08 23:15:54 +0330

    s

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layouts from './component/layouts';
+
+export default function App() {
+  return(
+    <div className="application">
+      <Layouts/>
+    </div>
+  )
+}
+
---
+++ src/component/layouts.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Layouts() {
+    return (
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae laborum odit fugiat voluptates eum consequatur sed dolores ut corporis quam nam similique temporibus, facere hic voluptatem accusamus voluptatibus animi laudantium.
+        </div>
+    )
+}
